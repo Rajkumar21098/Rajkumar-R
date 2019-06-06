bac5a6408d81926babc568276da4b631705ec1a4
Rajkumar21098 <51431557+Rajkumar21098@users.noreply.github.com>
2019-06-06 12:21:20 +0530

    Create test3.py

---
+++ test3.py
@@ -0,0 +1,5 @@
+Char=str(input("Enter the Character:"))
+if(Char=='a'or Char=='e'or Char=='i'or Char=='o'or Char=='u'):
+      print(Char,"The Character is vowel")
+else:
+      print(Char,"The character is consonant")
