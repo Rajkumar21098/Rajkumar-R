1c84c70a01fe051162e1b994a65140a204a1338c
Rajkumar21098 <51431557+Rajkumar21098@users.noreply.github.com>
2019-06-06 11:53:58 +0530

    Create test4.py

---
+++ test4.py
@@ -0,0 +1,5 @@
+Character=str(input("Enter the character:"))
+if((Character>='a'and Character<='z') or (Character>='A'and Character<='Z')):
+      print(Character,"is an alphabet")
+else:
+      print(Character,"is not an alphabet")
