0ee0c8477474388e310273d26064cd8df26698a2
Rajkumar21098 <51431557+Rajkumar21098@users.noreply.github.com>
2019-06-06 12:31:46 +0530

    Update test5.py

--- test5.py
+++ test5.py
@@ -1,6 +1,6 @@
-a=input("Enter the 1st number:")
-b=input("Enter the 2nd number:")
-c=input("Enter the 3rd number:")
+a=float(input("Enter the 1st number:"))
+b=float(input("Enter the 2nd number:"))
+c=float(input("Enter the 3rd number:"))
 if(a>b) and (a>c):
       print(a)
 elif(b>a) and (b>c):
