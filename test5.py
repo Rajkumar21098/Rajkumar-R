f6990a0984959899dc9f7673d2d2ff6b29236cc6
Rajkumar21098 <51431557+Rajkumar21098@users.noreply.github.com>
2019-06-06 12:28:24 +0530

    Create test5.py

---
+++ test5.py
@@ -0,0 +1,9 @@
+a=input("Enter the 1st number:")
+b=input("Enter the 2nd number:")
+c=input("Enter the 3rd number:")
+if(a>b) and (a>c):
+      print(a)
+elif(b>a) and (b>c):
+      print(b)
+else:
+      print(c)
