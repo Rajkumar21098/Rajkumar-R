299793e9e210791d17b7b86b42a0bf3acbcc88d2
Rajkumar21098 <51431557+Rajkumar21098@users.noreply.github.com>
2019-06-07 09:55:32 +0530

    Create test9.py

---
+++ test9.py
@@ -0,0 +1,11 @@
+n1=int(input("Enter the value of n1:"))
+k=int(input("Enter the value of k:"))
+m=[]
+s=0
+for i in range(n1):
+      y=input()
+      m.append(y)
+print(m)
+for i in range(k):
+      s=int(s)+int(m[i])
+print(s)
