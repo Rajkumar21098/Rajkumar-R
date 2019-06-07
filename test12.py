cc1f0355e428ed759817a5df7cda4cfaf73b29b4
Rajkumar21098 <51431557+Rajkumar21098@users.noreply.github.com>
2019-06-07 11:11:22 +0530

    Update test12.py

--- test12.py
+++ test12.py
@@ -5,6 +5,6 @@ while temp!=0:
 	rev=(rev*10)+(temp%10)
 	temp=temp//10 
 if number == rev:
-      print("number is palindrome")
+      print("YES")
 else:
-      print("number is not palindrome")
+      print("NO")
