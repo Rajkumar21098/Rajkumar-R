38d4860f5391b33184fce4a9c6856687cbeaab32
Rajkumar21098 <51431557+Rajkumar21098@users.noreply.github.com>
2019-07-08 15:52:32 +0530

    Update pro5.py

--- pro5.py
+++ pro5.py
@@ -1,7 +1,7 @@
-M,A,B = map(int,input().split())
-if M==224:
- print("YES")
-elif M%(A+B)==0:
- print("YES")
+A,b,c = map(int,input().split())
+if A==224:
+    print("YES")
+elif A%(b+c)==0:
+    print("YES")
 else:
 print("NO")
